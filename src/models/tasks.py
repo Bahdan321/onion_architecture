4af5098bc29f92bb0c148be5016a8098a5984647
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-01-21 16:24:17 +0200

    adding tasks model

--- src/models/tasks.py
+++ src/models/tasks.py
@@ -1,3 +1,19 @@
+from db.base import Base
+from schemas.tasks import TaskSchema
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 class Tasks(Base):
+
+    title: Mapped[str]
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    assignee_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    def to_read_model(self) -> TaskSchema:
+        return TaskSchema(
+            id=self.id,
+            title=self.title,
+            author_id=self.author_id,
+            assignee_id=self.assignee_id,
+        )
