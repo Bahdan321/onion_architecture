c93e55efff5104cc5d41f64a94756159c58f2519
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-01-21 16:28:02 +0200

    adding user model

---
+++ src/models/users.py
@@ -0,0 +1,14 @@
+from db.base import Base
+from schemas.users import UserSchema
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+class User(Base):
+
+    name: Mapped[str]
+
+    def to_read_moedl(self) -> UserSchema:
+        return UserSchema(
+            id=self.id,
+            name=self.name,
+        )
